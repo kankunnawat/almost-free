b3118656d3e0f517b20e4fa8fb04a1303d85cb43
kan.k <kan.k@siriustech.io>
2023-01-08 16:18:14 +0700

    comment package, team, testimonial section

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,9 @@ import ServiceSection from '../sections/service-section'
 import Feature from '../sections/feature'
 import CoreFeature from '../sections/core-feature'
 import WorkFlow from '../sections/workflow'
-import Package from '../sections/package'
-import TeamSection from '../sections/team-section'
-import TestimonialCard from '../sections/testimonial'
+// import Package from '../sections/package'
+// import TeamSection from '../sections/team-section'
+// import TestimonialCard from '../sections/testimonial'
 
 export default function IndexPage() {
 	return (
