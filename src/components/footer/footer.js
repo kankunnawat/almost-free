26df39a56a6e48e2c7040595cd0e958fa8f63a4b
kankunnawat <kan1657@gmail.com>
2023-01-13 15:48:19 +0700

    Test change repo name

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -13,17 +13,17 @@ export default function Footer() {
 						<Image src={FooterLogo} />
 					</Link>
 					{/* <Box sx={styles.footer.menus}>
-            <nav>
-              {data.menuItem.map((item, i) => (
-                <Link
-                  path={item.path}
-                  key={i}
-                  label={item.label}
-                  sx={styles.footer.link}
-                ></Link>
-              ))}
-            </nav>
-          </Box> */}
+						<nav>
+							{data.menuItem.map((item, i) => (
+								<Link
+									path={item.path}
+									key={i}
+									label={item.label}
+									sx={styles.footer.link}
+								></Link>
+							))}
+						</nav>
+					</Box> */}
 					<Text sx={styles.footer.copyright}>
 						Copyright by {new Date().getFullYear()} AlmostFree
 						Corporation
